25b0b0e47385f2dce6a0e44aff9260b2338da7c9
Sam <samb@tkc32m.com>
2023-09-20 21:23:32 -0500

    deploy

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,3 +1,3 @@
 ng build --configuration production
-aws s3 sync www s3://redi-research-web --delete --profile redi
+aws s3 sync dist/redi-research-template-web s3://redi-research-web --delete --profile redi
 aws cloudfront create-invalidation --distribution-id E2ZW03YBVYQ4U4 --paths "/*" --profile redi
